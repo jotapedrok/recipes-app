b9c9a930999569cc8c5ec17dd9b3c51bbf0a0a03
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2022-01-12 17:00:35 -0300

    estiliza perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import './style/Perfil.css';
 
 function Perfil() {
   const [email, setEmail] = useState();
@@ -26,32 +27,40 @@ function Perfil() {
   };
 
   return (
-    <div className="Perfil-content">
+    <div className="profile-container">
       <Header showSearch={ false } titlePage="Perfil" />
-      <h1 data-testid="profile-email">
-        {email}
-      </h1>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ () => history.push('/receitas-feitas') }
-      >
-        Receitas Feitas
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ () => history.push('/receitas-favoritas') }
-      >
-        Receitas Favoritas
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ () => resetLocalStorage() }
-      >
-        Sair
-      </button>
+      <div className="profile-content">
+        <h1
+          data-testid="profile-email"
+          className="profile-email"
+        >
+          {email}
+        </h1>
+        <button
+          className="profile-button"
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          className="profile-button"
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          className="profile-button"
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => resetLocalStorage() }
+        >
+          Sair
+        </button>
+      </div>
       <Footer />
     </div>
   );
